4aae52342270c549f469f08f17e09405298eac31
faizanrasyad <rasyadfaizan@gmail.com>
2024-08-17 23:47:42 +0700

    Initialized Home, Lihat & Buat Catatan

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.upload),
+      ),
+    );
   }
 }
---
+++ lib/pages/lihat_catatan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LihatCatatan extends StatefulWidget {
+  const LihatCatatan({super.key});
+
+  @override
+  State<LihatCatatan> createState() => _LihatCatatanState();
+}
+
+class _LihatCatatanState extends State<LihatCatatan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
